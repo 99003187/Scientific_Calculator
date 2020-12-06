ff6980d3823f828e5301786df09fb2bf60a28c25
99003188 <75420647+99003188@users.noreply.github.com>
2020-12-06 20:46:25 +0530

    Update calculator_operations.c

--- miniproject/3_Implementation/src/calculator_operations.c
+++ miniproject/3_Implementation/src/calculator_operations.c
@@ -123,3 +123,29 @@ int even_or_odd(int value)
         return 1; //for odd
     return 0;
 }
+double square(double input1)
+{
+    return(input1*input1);
+}
+double rectangle(double input1, double input2)
+{
+    return(input1*input2);
+}
+double cone(double input1,double input2)
+{
+    return(0.33*3.14*input1*input2*input1);
+}
+double cylinder(double input1,double input2)
+{
+    return(3.14*input1*input2*input1);
+}
+double BMI(double input1, double input2)
+{
+    if(input2>0)
+    return(input1/(input2*input2));
+    else if(input2<=0)
+    {
+    printf("enter correct details");
+    return 0;
+    }
+}
