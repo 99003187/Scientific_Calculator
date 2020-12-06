52331338d476a3648a1269a74adf35f461e5de5b
99003188 <75420647+99003188@users.noreply.github.com>
2020-12-06 20:45:17 +0530

    Update calculator_operations.h

--- miniproject/3_Implementation/inc/calculator_operations.h
+++ miniproject/3_Implementation/inc/calculator_operations.h
@@ -57,6 +57,41 @@ double squareroot(double value);
 int modulo(int x,int y);
 int even_or_odd(int value);
 int fact(int);
+double square(double input1);
+
+/**
+*  subtracts the operand1, operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return Result of operand1 - operand2
+*/
+double rectangle(double input1,double input2);
+
+/**
+*  multiply the operand1, operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return Result of operand1 * operand2
+*/
+double cone(double input1,double input2);
+
+
+/**
+* divides the operand1 by operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+double cylinder(double input1,double input2);
+/**
+* divides the operand1 by operand2 and returns the result
+* @param[in] operand1
+* @param[in] operand2
+* @return integer value of the operand1 / operand2
+* @note returns 0 for divide by 0 error
+*/
+double BMI(double input1,double input2);
 
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
